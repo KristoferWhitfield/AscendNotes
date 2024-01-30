b7a408eac21a5231a5cbda1c7db788fa37294438
Dionne <dionne.r.9@gmail.com>
2024-01-30 16:18:00 -0500

    add basic create and read operations

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,34 @@
 import express from "express";
+import { Note, mongoose } from "./utils/config.js";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 8080;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); // get information from html forms
 
 app.get("/", (req, res) => {
   res.send("Welcome to my server!");
 });
 
 app.get("/api/notes", (req, res) => {
-  let testNotes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      important: true,
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false,
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true,
-    },
-  ];
-  res.json(testNotes);
+  Note.find({}).then((result) => {
+    res.json(result);
+    mongoose.connection.close();
+    return result;
+  });
+});
+
+app.post("/api/note", (req, res) => {
+  const note = new Note({
+    content: req.body.content,
+    important: req.body.important,
+  });
+  note.save().then((result) => {
+    res.json(result);
+    mongoose.connection.close();
+    return result;
+  });
 });
 
 app.listen(port, () => {
