95f2596ff4a8a6a3c021da7920539caded9cdba7
Dionne <dionne.r.9@gmail.com>
2024-01-31 15:31:16 -0500

    refactor server and setup initial test

--- backend/models/note.js
+++ backend/models/note.js
@@ -1,5 +1,20 @@
 import mongoose from "mongoose";
-export const noteSchema = new mongoose.Schema({
-  content: String,
+
+const noteSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    minlength: 5,
+    required: true,
+  },
   important: Boolean,
 });
+
+noteSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+export const Note = mongoose.model("Note", noteSchema);
